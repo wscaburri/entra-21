7e340c36143541aa795d132672360a80199a7fb5
Wellington Scaburri <wscaburri@gmail.com>
2022-05-26 21:55:31 -0300

    Acrescentado Exercícios Orientação Objeto

---
+++ Entra21.ExerciciosOrientacaoObjeto/Exercicio01/Livro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio01
+{
+    public class Livro
+    {
+        string tituloLivro;
+        string autorLivro;
+        string codigoIsbn;
+        string idiomaOriginal;
+        DateTime dataLancamento;
+        int quantidadePaginas;
+        int quantidadePaginasLidas;
+        int quantidadeReleituras;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine("Título: " + tituloLivro);
+        }
+
+        public void ApresentarQuantidadePaginasParaLer()
+        {
+            var quantidadePaginasParaLer = quantidadePaginas - quantidadePaginasLidas;
+            Console.WriteLine(quantidadePaginasParaLer);
+        }
+
+        public void ApresentarQuantidadePaginasLidasNoTotal()
+        {
+            Console.WriteLine("Total de páginas lidas: " + quantidadePaginasLidas);
+        }
+
+    }
+}
