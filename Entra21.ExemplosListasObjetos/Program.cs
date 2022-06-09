41a8cb6470cca6fdced35fd7ab71ee04d43a8f6d
wscaburri <wscaburri@gmail.com>
2022-06-08 22:07:26 -0300

    Adicionado Program

--- Entra21.ExemplosListasObjetos/Program.cs
+++ Entra21.ExemplosListasObjetos/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entra21.ExemplosListasObjetos;
+
+ProdutoControlador controlador = new ProdutoControlador();
+controlador.GerenciarMenu();
