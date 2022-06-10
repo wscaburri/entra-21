9a9374289ca6f4889361be7ad025e2be76121cf2
wscaburri <wscaburri@gmail.com>
2022-06-09 21:33:08 -0300

    Exemplo Concluido

--- Entra21.ExemplosListasObjetos/ProdutoControlador.cs
+++ Entra21.ExemplosListasObjetos/ProdutoControlador.cs
@@ -12,10 +12,153 @@ namespace Entra21.ExemplosListasObjetos
 
         public void GerenciarMenu()
         {
-            Cadastrar();
+            int codigo = 0;
+
+            // Repete enquanto o código não for o menu sair(6)
+            while (codigo != 6)
+            {
+                Console.Clear();
+                // Apresenta o menu e solicita o código
+                codigo = ApresentarSolicitarMenu();
+
+                Console.Clear();
+
+                if (codigo == 1)
+                {
+                    // Menu escolhido para listar produtos
+                    ApresentarProdutos();
+                }
+                else if (codigo == 2)
+                {
+                    // Menu escolhido para cadastrar produtos
+                    Cadastrar();
+                }
+                else if (codigo == 3)
+                {
+                    // Menu escolhido para editar produtos
+                    Editar();
+                }
+                else if (codigo == 4)
+                {
+                    Apagar();
+                }
+                else if (codigo == 5)
+                {
+                    ApresentarProduto();
+                }
+
+                Console.Write("\n\nAperte alguma tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+
+        private void ApresentarProduto()
+        {
             ApresentarProdutos();
-            Cadastrar();
+
+            Console.Write("Digite o código do produto a ser detalhado: ");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+
+            var produto = produtoServico.ObterPorCodigo(codigo);
+
+            // Verifica se o produto não está cadastrado na lista de produtos
+            if (produto == null)
+            {
+                Console.WriteLine("Produto não cadastrado");
+
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine($@"Código: {produto.Codigo}
+Nome: {produto.Nome}
+Preço Unitário: {produto.PrecoUnitario}
+Quantidade: {produto.Quantidade}
+Total: {produto.CalcularPrecoTotal()}");
+        }
+
+        private void Apagar()
+        {
             ApresentarProdutos();
+
+            Console.WriteLine("Digite o código do produto para apagar: ");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+
+            var registroApagado = produtoServico.Apagar(codigo);
+
+            Console.WriteLine(registroApagado == true
+                ? "Registro removido com sucesso"
+                : "Nenhum produto cadastrado com o código informado");
+        }
+
+        private void Editar()
+        {
+            ApresentarProdutos();
+
+            Console.WriteLine("Código do produto desejado: ");
+            var codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+
+            Console.Write("Quantidade: ");
+            var quantidade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Preço Unitário: ");
+            var precoUnitario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(@"Localizações disponíveis:
+- Armazem
+- Area Venda
+- Loja");
+            Console.Write("Localização: ");
+            var localizacao = Console.ReadLine();
+
+            var localizacaoProduto = ObterLocalizacaoProduto(localizacao);
+
+            var alterou = produtoServico.Editar(codigo, nome, precoUnitario, localizacaoProduto, quantidade);
+
+            if (alterou == false)
+            {
+                Console.WriteLine("Código digitado não existe");
+            }
+            else
+            {
+                Console.WriteLine("Produto alterado com sucesso");
+            }
+        }
+
+        private int ApresentarSolicitarMenu()
+        {
+            Console.WriteLine(@" MENU:
+1 - Listar todos
+2 - Cadastrar
+3 - Editar
+4 - Apagar
+5 - Apresentar produto desejado
+6 - Sair");
+
+            int codigo = SolicitarCodigo();
+            return codigo;
+        }
+
+        private int SolicitarCodigo()
+        {
+            int codigo = 0;
+            // Continua solicitando o código até que um seja um código entre 1 e 6
+            while (codigo < 1 || codigo > 6)
+            {
+                try
+                {
+                    Console.Write("Digite o código: ");
+                    codigo = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Digite um menu válido");
+                }
+            }
+            return codigo;
         }
 
         private void Cadastrar()
@@ -29,6 +172,10 @@ namespace Entra21.ExemplosListasObjetos
             Console.Write("Preço unitário: ");
             var precoUnitario = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(@"Localizações disponíveis:
+- Armazem
+- Area Venda
+- Loja");
             Console.Write("Localização: ");
             var localizacao = Console.ReadLine();
 
@@ -57,12 +204,24 @@ namespace Entra21.ExemplosListasObjetos
         {
             var produtos = produtoServico.ObterTodos();
 
-            for(var i = 0; i < produtos.Count; i++)
+            if (produtos.Count == 0)
+            {
+                Console.WriteLine("Nenhum produto cadastrado :( ");
+                // Parar a execução deste método
+                return;
+            }
+
+            Console.WriteLine("Lista de Produtos");
+
+            for (var i = 0; i < produtos.Count; i++)
             {
                 var produtoAtual = produtos[i];
 
-                Console.WriteLine("Nome: " + produtoAtual.Nome + " Preço unitário: R$" + produtoAtual.PrecoUnitario);
+                Console.WriteLine(
+                    "\nCódigo" + produtoAtual.Codigo +
+                    "\nNome: " + produtoAtual.Nome + "\n");
             }
         }
+
     }
 }
--- Entra21.ExemplosListasObjetos/ProdutoServico.cs
+++ Entra21.ExemplosListasObjetos/ProdutoServico.cs
@@ -9,6 +9,10 @@ namespace Entra21.ExemplosListasObjetos
     internal class ProdutoServico
     {
         private List<Produto> produtos = new List<Produto>();
+
+        // Armazenar o código do próximo produto
+        private int codigoAtual = 1;
+
         // encapsulamento + tipoRetorno + NomeMetodo(parametros)
         public void Adicionar(string nome, double precoUnitario, ProdutoLocalizacao localizacao, int quantidade)
         {
@@ -21,18 +25,66 @@ namespace Entra21.ExemplosListasObjetos
             produto.Localizacao = localizacao;
             produto.Quantidade = quantidade;
 
+            // Atribuir o código novo para o objeto do produto
+            produto.Codigo = codigoAtual;
+
+            // Adicionar 1 no código atual para quando criar um novo produto o produto conter outro código
+            codigoAtual = codigoAtual + 1;
+
             // Adicionar o produto instanciado na lista de produtos
             produtos.Add(produto);
         }
 
-        public void Editar()
+        public bool Editar(
+            int codigoParaAlterar,
+            string nome,
+            double precoUnitario,
+            ProdutoLocalizacao localizacao,
+            int quantidade)
         {
+            // Obtem o produto desejado da lista de produto
+            Produto produtoParaAlterar = ObterPorCodigo(codigoParaAlterar);
 
+            // Verifica se não foi possível encontrar o produto
+            if (produtoParaAlterar == null)
+            {
+                // Retorna falso pq não existe produto com o código dos parâmetros
+                return false;
+            }
+
+            // Atualiza as propriedades (campos) do produto desejado
+            produtoParaAlterar.Nome = nome;
+            produtoParaAlterar.Localizacao = localizacao;
+            produtoParaAlterar.PrecoUnitario = precoUnitario;
+            produtoParaAlterar.Quantidade = quantidade;
+
+            // Retorna verdadeiro pq foi possível alterar o produto
+            return true;
         }
 
-        public void Apagar()
+        public bool Apagar(int codigo)
         {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Resgatando de uma lista de tipos primitivos
+                // int numero = numeros[i];
 
+                // Resgatando de uma lista de objetos
+                Produto produto = produtos[i];
+
+                if (produto.Codigo == codigo)
+                {
+                    // Remover com o indice do objeto desejado
+                    //produtos.RemoveAt(i);
+
+                    // Remover com o objeto desejado
+                    produtos.Remove(produto);
+                    // Retorna verdadeiro pq o produto com o código desejado foi removido da lista
+                    return true;
+                }
+            }
+            // Retorna falso pq não foi encontrado produto com o código desejado, ou seja, nenhum elemento foi removido da lista
+            return false;
         }
 
         public List<Produto> ObterTodos()
@@ -40,8 +92,22 @@ namespace Entra21.ExemplosListasObjetos
             return produtos;
         }
 
-        public Produto ObterPorCodigo()
+        public Produto ObterPorCodigo(int codigo)
         {
+            // Percorrer todos os elementos para encontrar o produto filtrando por código
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Obter o produto da lista de produtos em determinado indice
+                var produtoAtual = produtos[i];
+
+                // Verificar se o produto atual da lista percorrida é o desejado
+                if (produtoAtual.Codigo == codigo)
+                {
+                    // Retorna o produto encontrado
+                    return produtoAtual;
+                }
+            }
+            // Retorna null, caso não encontre um produto com o código passado como parâmetro
             return null;
         }
     }
