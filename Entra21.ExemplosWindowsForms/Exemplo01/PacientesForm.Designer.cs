19a376a342619d91269785f6cad021e4ff047e81
Wellington Scaburri <wscaburri@gmail.com>
2022-06-15 22:20:36 -0300

    Adicionado Exemplo Windows Formas

---
+++ Entra21.ExemplosWindowsForms/Exemplo01/PacientesForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Entra21.ExemplosWindowsForms.Exemplo01
+{
+    partial class PacientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelAltura = new System.Windows.Forms.Label();
+            this.textBoxAltura = new System.Windows.Forms.TextBox();
+            this.labelPeso = new System.Windows.Forms.Label();
+            this.textBoxPeso = new System.Windows.Forms.TextBox();
+            this.labelImc = new System.Windows.Forms.Label();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAltura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPeso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnImc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(407, 12);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(80, 42);
+            this.buttonApagar.TabIndex = 0;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            this.buttonApagar.Click += new System.EventHandler(this.buttonApagar_Click);
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(321, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(80, 42);
+            this.buttonEditar.TabIndex = 1;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            this.buttonEditar.Click += new System.EventHandler(this.buttonEditar_Click);
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(518, 81);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 2;
+            this.labelNome.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(518, 99);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(252, 23);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // labelAltura
+            // 
+            this.labelAltura.AutoSize = true;
+            this.labelAltura.Location = new System.Drawing.Point(518, 137);
+            this.labelAltura.Name = "labelAltura";
+            this.labelAltura.Size = new System.Drawing.Size(39, 15);
+            this.labelAltura.TabIndex = 4;
+            this.labelAltura.Text = "Altura";
+            // 
+            // textBoxAltura
+            // 
+            this.textBoxAltura.Location = new System.Drawing.Point(518, 155);
+            this.textBoxAltura.Name = "textBoxAltura";
+            this.textBoxAltura.Size = new System.Drawing.Size(252, 23);
+            this.textBoxAltura.TabIndex = 5;
+            // 
+            // labelPeso
+            // 
+            this.labelPeso.AutoSize = true;
+            this.labelPeso.Location = new System.Drawing.Point(518, 194);
+            this.labelPeso.Name = "labelPeso";
+            this.labelPeso.Size = new System.Drawing.Size(32, 15);
+            this.labelPeso.TabIndex = 6;
+            this.labelPeso.Text = "Peso";
+            // 
+            // textBoxPeso
+            // 
+            this.textBoxPeso.Location = new System.Drawing.Point(518, 212);
+            this.textBoxPeso.Name = "textBoxPeso";
+            this.textBoxPeso.Size = new System.Drawing.Size(251, 23);
+            this.textBoxPeso.TabIndex = 7;
+            // 
+            // labelImc
+            // 
+            this.labelImc.AutoSize = true;
+            this.labelImc.Location = new System.Drawing.Point(518, 264);
+            this.labelImc.Name = "labelImc";
+            this.labelImc.Size = new System.Drawing.Size(32, 15);
+            this.labelImc.TabIndex = 8;
+            this.labelImc.Text = "IMC:";
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(638, 251);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(132, 40);
+            this.buttonSalvar.TabIndex = 9;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnCodigo,
+            this.ColumnNome,
+            this.ColumnAltura,
+            this.ColumnPeso,
+            this.ColumnImc});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(498, 349);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // ColumnCodigo
+            // 
+            this.ColumnCodigo.HeaderText = "Código";
+            this.ColumnCodigo.Name = "ColumnCodigo";
+            this.ColumnCodigo.ReadOnly = true;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.ReadOnly = true;
+            // 
+            // ColumnAltura
+            // 
+            this.ColumnAltura.HeaderText = "Altura";
+            this.ColumnAltura.Name = "ColumnAltura";
+            this.ColumnAltura.ReadOnly = true;
+            // 
+            // ColumnPeso
+            // 
+            this.ColumnPeso.HeaderText = "Peso";
+            this.ColumnPeso.Name = "ColumnPeso";
+            this.ColumnPeso.ReadOnly = true;
+            // 
+            // ColumnImc
+            // 
+            this.ColumnImc.HeaderText = "IMC";
+            this.ColumnImc.Name = "ColumnImc";
+            this.ColumnImc.ReadOnly = true;
+            // 
+            // PacientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.labelImc);
+            this.Controls.Add(this.textBoxPeso);
+            this.Controls.Add(this.labelPeso);
+            this.Controls.Add(this.textBoxAltura);
+            this.Controls.Add(this.labelAltura);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonApagar);
+            this.Name = "PacientesForm";
+            this.Text = "PacientesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonApagar;
+        private Button buttonEditar;
+        private Label labelNome;
+        private TextBox textBoxNome;
+        private Label labelAltura;
+        private TextBox textBoxAltura;
+        private Label labelPeso;
+        private TextBox textBoxPeso;
+        private Label labelImc;
+        private Button buttonSalvar;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColumnCodigo;
+        private DataGridViewTextBoxColumn ColumnNome;
+        private DataGridViewTextBoxColumn ColumnAltura;
+        private DataGridViewTextBoxColumn ColumnPeso;
+        private DataGridViewTextBoxColumn ColumnImc;
+    }
+}
---
+++ Entra21.ExemplosWindowsForms/Exemplo01/PacientesForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entra21.ExemplosWindowsForms.Exemplo01
+{
+    public partial class PacientesForm : Form
+    {
+        private int codigo = 1;
+        private int indiceLinhaSelecionada = -1;
+        private int codigoSelecionado = -1;
+
+        public PacientesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            // Obter as informações dos campos
+            var nome = textBoxNome.Text.Trim();
+            var altura = Convert.ToDouble(textBoxAltura.Text.Trim());
+            var peso = Convert.ToDouble(textBoxPeso.Text.Trim());
+
+            // Calcular Imc
+            var imc = peso / (altura * altura);
+
+            // Verifica se está em modo de adição
+            if (indiceLinhaSelecionada == -1)
+            {
+                // Adicionar linha no dataGridView de pacientes
+                dataGridView1.Rows.Add(new object[]
+                {
+                codigo++, nome, altura, peso, imc
+                });
+
+                return;
+            }
+
+            dataGridView1.Rows[indiceLinhaSelecionada].Cells[1].Value = nome;
+            dataGridView1.Rows[indiceLinhaSelecionada].Cells[1].Value = altura.ToString();
+            dataGridView1.Rows[indiceLinhaSelecionada].Cells[3].Value = peso.ToString();
+            dataGridView1.Rows[indiceLinhaSelecionada].Cells[4].Value = imc.ToString();
+        }
+
+        private void buttonApagar_Click(object sender, EventArgs e)
+        {
+            var indiceLinhaSelecionada = dataGridView1.SelectedRows[0].Index;
+
+            if (indiceLinhaSelecionada == -1)
+            {
+                MessageBox.Show("Selecione um Paciente");
+                return;
+            }
+
+            var opcaoEscolhida = MessageBox.Show("Deseja realmente apagar?", "Aviso", MessageBoxButtons.YesNo);
+
+            // Verifica se o usuário escolheu realmente apagar o registro
+            if (opcaoEscolhida == DialogResult.Yes)
+            {
+                // Remove a linha utilizando o indice do DataGridView
+                dataGridView1.Rows.RemoveAt(indiceLinhaSelecionada);
+            }
+        }
+
+        private void buttonEditar_Click(object sender, EventArgs e)
+        {
+            indiceLinhaSelecionada = dataGridView1.SelectedRows[0].Index;
+
+            if (indiceLinhaSelecionada == -1)
+            {
+                MessageBox.Show("Selecione um paciente");
+                return; 
+            }
+
+            // Obter a linha que o usuario selecionou
+            var linhaSelecionada = dataGridView1.SelectedRows[0];
+
+            // Obter a informação da linha selecionada passando a coluna desejada
+            codigoSelecionado = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
+            var nome = linhaSelecionada.Cells[1].Value.ToString();
+            var altura = Convert.ToDouble(linhaSelecionada.Cells[2].Value);
+            var peso = Convert.ToDouble(linhaSelecionada.Cells [3].Value); 
+
+            textBoxNome.Text = nome;
+            textBoxAltura.Text =  altura.ToString();
+            textBoxPeso.Text = peso.ToString();
+        }
+    }
+}
