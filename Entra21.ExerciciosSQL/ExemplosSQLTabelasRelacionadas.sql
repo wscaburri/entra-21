1967544914c5103946577ad267539ff7e249312d
Wellington Scaburri <wscaburri@gmail.com>
2022-07-11 21:20:02 -0300

    Adicionado ExemploSQL - Tabelas Relacionadas

---
+++ Entra21.ExerciciosSQL/ExemplosSQLTabelasRelacionadas.sql
@@ -0,0 +1,71 @@
+﻿CREATE TABLE pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	tipo INTEGER,
+	preco_unitario DECIMAL(6,2));
+
+CREATE TABLE clientes(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(40),
+	cpf VARCHAR(14));
+
+CREATE TABLE enderecos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), -- PK
+	id_cliente INTEGER NOT NULL, -- FK
+	cep VARCHAR(9) NOT NULL,
+	estado VARCHAR(2) NOT NULL,
+	cidade VARCHAR(30) NOT NULL,
+	bairro VARCHAR(30) NOT NULL,
+	logradouro VARCHAR(40) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	descricao TEXT,
+	-- FOREIGN KEY nome_coluna_tabela_atual REFERENCES nome_tabela_pai(nome_coluna_pai_pk))
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id));
+
+CREATE TABLE pedidos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	id_cliente INTEGER NOT NULL,
+	status TINYINT NOT NULL, -- 0...255,
+	data_criacao DATETIME2 NOT NULL,
+	data_efetivaco_compra DATETIME2
+	);
+
+CREATE TABLE pedidos_pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), -- PK
+	id_peca INTEGER, -- FK tabela peças
+	id_pedido INTEGER, -- FK tabela pedido
+	quantidade SMALLINT
+	FOREIGN KEY(id_pedido) REFERENCES pedidos(id),
+	FOREIGN KEY(id_peca) REFERENCES pecas(id)
+	); 
+
+-- TIPOS DE PEÇAS
+-- 1 = SSD
+-- 2 = Placa de Vídeo
+-- 3 = Placa Mãe
+-- 4 = Fonte
+-- 5 = Memória RAM
+
+INSERT INTO pecas(nome, tipo, preco_unitario) VAlUES
+('SSD 240 GB', 1, 200),
+('SSD 240 M2', 1, 420.39),
+('RTX 3090 TI', 2, 9999.90),
+('GTX 1060', 2, 1500),
+('16GB DDR5 4800Ghz', 5, 800),
+('16GB DDR4 3200Ghz', 5, 350);
+
+INSERT INTO clientes(nome, cpf) VALUES
+('Claudio', '070.355.489-73'),
+('Cry', '032.599.384-69'),
+('Juliana', '123.495.392-02');
+
+INSERT INTO enderecos(id_cliente, estado, cidade, bairro, cep, logradouro, numero) VALUES
+(1, 'SC', 'Blumenau', 'Velha', '89070-301', 'Rua Divinópolis', 777),
+(2, 'SC', 'Blumenau', 'Velha Grande', '89070-472', 'Morro da Edith', 36),
+(3, 'SC', 'Blumenau', 'Judity', 'Sem Cep', 'Rua da Kellen', 23);
+
+SELECT * FROM pecas;
+SELECT * FROM clientes;
+SELECT * FROM enderecos;
+
+
