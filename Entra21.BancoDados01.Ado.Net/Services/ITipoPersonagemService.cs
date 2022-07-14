cdf2c68e8ebf55814cd97f4b4880cabbc7d090c8
Wellington Scaburri <wscaburri@gmail.com>
2022-07-14 19:40:06 -0300

    Criado Exemplo BancoDados1

---
+++ Entra21.BancoDados01.Ado.Net/Services/ITipoPersonagemService.cs
@@ -0,0 +1,14 @@
+﻿using Entra21.BancoDados01.Ado.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados01.Ado.Net.Services
+{
+    internal interface ITipoPersonagemService
+    {
+        void Cadastrar(TipoPersonagem tipoPersonagem);
+    }
+}
---
+++ Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
@@ -0,0 +1,26 @@
+﻿using Entra21.BancoDados01.Ado.Net.Models;
+using System.Data.SqlClient;
+
+namespace Entra21.BancoDados01.Ado.Net.Services
+{
+    // Dois pontos seguido de uma interface, quer dizer que a classe será obrigada a implementar os métodos(contratos) estabelecidos na interface
+    internal class TipoPersonagemService : ITipoPersonagemService
+    {
+        public void Cadastrar(TipoPersonagem tipoPersonagem)
+        {
+            //Será descrito a implementação do contrato definido na interface            
+
+            // Instanciado um objeto da classe SqlConnection, que permitirá fazer selects, inserts, updates, deletes, etc.
+            SqlConnection conexao = new SqlConnection();
+
+            // string que contém o caminho para o banco de dados, o que permitirá conectar ao banco de dados.
+            var connectionString = @"";
+
+            // Definir o caminho da conexão para o SqlConnection
+            conexao.ConnectionString = connectionString;
+
+            // Abrir a conexão com o bando de dados
+            conexao.Open();
+        } 
+    }
+}
