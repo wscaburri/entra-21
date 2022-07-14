e2d12996f17dada01476ed1efb6fdccb2679caac
Wellington Scaburri <wscaburri@gmail.com>
2022-07-14 20:22:57 -0300

    Atualizado Exemplo

--- Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
+++ Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
@@ -14,13 +14,24 @@ namespace Entra21.BancoDados01.Ado.Net.Services
             SqlConnection conexao = new SqlConnection();
 
             // string que contém o caminho para o banco de dados, o que permitirá conectar ao banco de dados.
-            var connectionString = @"";
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\\Server\c#-noturno\wellington.scaburri\Desktop\ExemploBancoDados01AdoNet.mdf;Integrated Security=True;Connect Timeout=30";
 
             // Definir o caminho da conexão para o SqlConnection
             conexao.ConnectionString = connectionString;
 
             // Abrir a conexão com o bando de dados
             conexao.Open();
+
+            // Criar o comando para executar no banco de dados
+            SqlCommand comando = conexao.CreateCommand();
+
+            // Especificando o comando que será executado
+            comando.CommandText = "INSERT INTO tipos_personagens (tipo) VALUES ('" + tipoPersonagem.Tipo + "')";
+
+            // Executando o comando de insert na tabela de tipos personagens
+            comando.ExecuteNonQuery();
+
+            conexao.Close();
         } 
     }
 }
---
+++ Entra21.BancoDados01.Ado.Net/Views/TiposPersonagens/TipoPersonagemCadastroEdicaoForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Entra21.BancoDados01.Ado.Net.Views.TiposPersonagens
+{
+    partial class TipoPersonagemCadastroEdicaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTipo = new System.Windows.Forms.Label();
+            this.textBoxTipo = new System.Windows.Forms.TextBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTipo
+            // 
+            this.labelTipo.AutoSize = true;
+            this.labelTipo.Location = new System.Drawing.Point(38, 18);
+            this.labelTipo.Name = "labelTipo";
+            this.labelTipo.Size = new System.Drawing.Size(39, 20);
+            this.labelTipo.TabIndex = 0;
+            this.labelTipo.Text = "Tipo";
+            // 
+            // textBoxTipo
+            // 
+            this.textBoxTipo.Location = new System.Drawing.Point(38, 41);
+            this.textBoxTipo.Name = "textBoxTipo";
+            this.textBoxTipo.Size = new System.Drawing.Size(356, 27);
+            this.textBoxTipo.TabIndex = 1;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(300, 74);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(94, 29);
+            this.buttonSalvar.TabIndex = 2;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(200, 74);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(94, 29);
+            this.buttonCancelar.TabIndex = 3;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // TipoPersonagemCadastroEdicaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 131);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.textBoxTipo);
+            this.Controls.Add(this.labelTipo);
+            this.Name = "TipoPersonagemCadastroEdicaoForm";
+            this.Text = "Cadastro/Edição Tipo de Personagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelTipo;
+        private TextBox textBoxTipo;
+        private Button buttonSalvar;
+        private Button buttonCancelar;
+    }
+}
