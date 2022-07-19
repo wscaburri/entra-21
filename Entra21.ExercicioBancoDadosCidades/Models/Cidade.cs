1013d4b3c3e7b2e7708d31139b0bb476ac08d5b8
Wellington Scaburri <wscaburri@gmail.com>
2022-07-18 22:11:26 -0300

    Criado Exercicio BancoDados Cidades

---
+++ Entra21.ExercicioBancoDadosCidades/Models/Cidade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioBancoDadosCidades.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+
+        public UnidadeFederativa UnidadeFederativa { get; set; }
+
+        public string Nome { get; set; }
+
+        public int QuandidadeHabitantes { get; set; }
+
+        public DateTime DataHoraFundacao { get; set; }
+
+        public double Pib { get; set; }
+    }
+}
---
+++ Entra21.ExercicioBancoDadosCidades/Models/UnidadeFederativa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioBancoDadosCidades.Models
+{
+    public class UnidadeFederativa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sigla { get; set; }
+    }
+}
