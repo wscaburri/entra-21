db10179033b2acdfb48a5189766dce0bd06be49a
Wellington Scaburri <wscaburri@gmail.com>
2022-07-20 22:34:20 -0300

    Unidade Federativa

--- Entra21.ExercicioBancoDadosCidades/View/UnidadeFederativaListagemForm.Designer.cs
+++ Entra21.ExercicioBancoDadosCidades/View/UnidadeFederativaListagemForm.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnSigla = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnNome,
+            this.ColumnSigla});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(451, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(242, 25);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 1;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            this.buttonApagar.Click += new System.EventHandler(this.buttonApagar_Click);
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(323, 25);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 2;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            this.buttonEditar.Click += new System.EventHandler(this.buttonEditar_Click);
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(404, 25);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCadastrar.TabIndex = 3;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "Código";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.ReadOnly = true;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.ReadOnly = true;
+            // 
+            // ColumnSigla
+            // 
+            this.ColumnSigla.HeaderText = "Sigla";
+            this.ColumnSigla.Name = "ColumnSigla";
+            this.ColumnSigla.ReadOnly = true;
+            // 
+            // UnidadeFederativaListagemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UnidadeFederativaListagemForm";
+            this.ClientSize = new System.Drawing.Size(512, 401);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "UnidadeFederativaListagemForm";
+            this.Text = "Unidades Federativas";
+            this.Load += new System.EventHandler(this.UnidadeFederativaListagemForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button buttonApagar;
+        private Button buttonEditar;
+        private Button buttonCadastrar;
+        private DataGridViewTextBoxColumn ColumnId;
+        private DataGridViewTextBoxColumn ColumnNome;
+        private DataGridViewTextBoxColumn ColumnSigla;
     }
 }
--- Entra21.ExercicioBancoDadosCidades/View/UnidadeFederativaListagemForm.cs
+++ Entra21.ExercicioBancoDadosCidades/View/UnidadeFederativaListagemForm.cs
@@ -1,20 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Entra21.ExercicioBancoDadosCidades.Services;
 
 namespace Entra21.ExercicioBancoDadosCidades.View
 {
     public partial class UnidadeFederativaListagemForm : Form
     {
+        private UnidadeFederativaService unidadeFederativaService;
+
         public UnidadeFederativaListagemForm()
         {
             InitializeComponent();
+
+            unidadeFederativaService = new UnidadeFederativaService();
+        }
+
+        private void AtualizarRegistrosDataGridView()
+        {
+            var unidadesFederativas = unidadeFederativaService.ObterTodos();
+
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < unidadesFederativas.Count; i++)
+            {
+                var unidadeFederativa = unidadesFederativas[i];
+
+                dataGridView1.Rows.Add(new object[]
+                {
+                    unidadeFederativa.Id,
+                    unidadeFederativa.Nome,
+                    unidadeFederativa.Sigla
+                });
+            }
+        }
+
+        private void UnidadeFederativaListagemForm_Load(object sender, EventArgs e)
+        {
+            AtualizarRegistrosDataGridView();
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            var unidadeFederativaCadastroEdicaoForm = new UnidadeFederativaCadastroEdicaoForm();
+
+            unidadeFederativaCadastroEdicaoForm.ShowDialog();
+
+            AtualizarRegistrosDataGridView();
+        }
+
+        private void buttonApagar_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+
+            unidadeFederativaService.Apagar(id);
+
+            AtualizarRegistrosDataGridView();
+
+            MessageBox.Show("Unidade Federativa apagada com sucesso!");
+        }
+
+        private void buttonEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Cadastre alguma Unidade Federativa!");
+                return;
+            }
+
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione alguma Unidade Federativa!");
+                return;
+            }
+
+            var linhaSelecionada = dataGridView1.SelectedRows[0];
+
+            var id = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
+
+            var unidadeFederativa = unidadeFederativaService.ObterPorId(id);
+
+            var unidadeFederativaForm = new UnidadeFederativaCadastroEdicaoForm(unidadeFederativa);
+
+            // apresentado o form para o usuário
+            unidadeFederativaForm.ShowDialog();
+
+            AtualizarRegistrosDataGridView();
         }
     }
 }
