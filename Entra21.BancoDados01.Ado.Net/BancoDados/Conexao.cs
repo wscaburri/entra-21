0d20a661e2bc8b2cc6248b2a57d6b032f63fb3f2
Wellington Scaburri <wscaburri@gmail.com>
2022-07-31 21:01:46 -0300

    Atualização conexão

--- Entra21.BancoDados01.Ado.Net/BancoDados/Conexao.cs
+++ Entra21.BancoDados01.Ado.Net/BancoDados/Conexao.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
 namespace Entra21.BancoDados01.Ado.Net.BancoDados
 {
@@ -15,7 +10,7 @@ namespace Entra21.BancoDados01.Ado.Net.BancoDados
             SqlConnection conexao = new SqlConnection();
 
             // string que contém o caminho para o banco de dados, o que permitirá conectar ao banco de dados.
-            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\\Server\c#-noturno\wellington.scaburri\Desktop\Entra21BancoDados01AdoNet.mdf;Integrated Security=True;Connect Timeout=30";
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\wscab\source\repos\BancoDados.mdf;Integrated Security=True;Connect Timeout=30";
 
             // Definir o caminho da conexão para o SqlConnection
             conexao.ConnectionString = connectionString;
